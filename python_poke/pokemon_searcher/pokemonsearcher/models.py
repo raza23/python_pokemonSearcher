21ce957147a1af0bfb543fc0b52c2817a83060ec
Raza <rtshareef@gmail.com>
2020-03-31 11:47:31 -0400

    model added

--- python_poke/pokemon_searcher/pokemonsearcher/models.py
+++ python_poke/pokemon_searcher/pokemonsearcher/models.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    fronturl = models.CharField(max_length=100)
+    backurl = models.CharField(max_length=100)
+    hp = models.IntegerField()
