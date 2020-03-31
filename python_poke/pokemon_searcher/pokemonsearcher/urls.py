02c03744374b85473153c4880a633618f2db7550
Raza <rtshareef@gmail.com>
2020-03-31 11:15:30 -0400

    user created

--- python_poke/pokemon_searcher/pokemonsearcher/urls.py
+++ python_poke/pokemon_searcher/pokemonsearcher/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.conf.urls import url, include
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url('', include('pokemonsearcher.urls')),
+
 ]
